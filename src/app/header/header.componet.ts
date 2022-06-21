0ae5d0d1e18cc4c9c1749e95e64390f75ea125a2
francisco <fruedagit@gmail.com>
2022-06-21 13:05:29 +0200

    mejorafrontbootstrap

--- src/app/header/header.componet.ts
+++ src/app/header/header.componet.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html' //Llamada al componente html
 })
 
 export class HeaderComponent {

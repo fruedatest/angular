fdb6135c8c4c3cc31747a6c20865f15632398e57
francisco <fruedagit@gmail.com>
2022-06-21 13:28:32 +0200

    directivaIf

--- src/app/directiva/directiva.component.ts
+++ src/app/directiva/directiva.component.ts
@@ -10,8 +10,13 @@ import { Component, OnInit } from '@angular/core';
 export class DirectivaComponent {
 
   lista: string[] = ['TypeScript', 'Java', 'C++', 'C#', 'PHP'];
-  
+
+  habilitar: boolean = true;
+
   constructor() { }
 
+  setHabilitar(): void{
+    this.habilitar = (this.habilitar == true)? false: true;
+  }
   
 }

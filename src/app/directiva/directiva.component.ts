cb79bcfaaf9f4ff21c042b58cc6bb1dc2f5d1efc
francisco <fruedagit@gmail.com>
2022-06-21 13:18:13 +0200

    directivaFor

---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html',
+
+})
+
+
+export class DirectivaComponent {
+
+  lista: string[] = ['TypeScript', 'Java', 'C++', 'C#', 'PHP'];
+  
+  constructor() { }
+
+  
+}

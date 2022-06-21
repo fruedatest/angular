c95df9fa964be24309cd77d2b955ee62e5b01731
francisco <fruedagit@gmail.com>
2022-06-21 12:44:28 +0200

    refactorizacion header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
+
+
 export class AppComponent {
   title:string = 'my-app';
   autor:string = 'francisco';
   year:string = '2022';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header.componet';
+import { HeaderComponent } from './header/header.componet';
 
 @NgModule({
   declarations: [
